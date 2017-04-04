d3c04680a3670381555817ea8c6a917b5057a50c
Ruhul Amin <ruhul@onfido.com>
2017-04-04 16:35:03 +0100

    Add main

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import factory as factory
 
-print(factory.get_type("A"))
+if __name__== "__main__":
+    print(factory.get_type("A"))
+    print(factory.get_type("B"))
+    print(factory.get_type("C"))
 
-print(factory.get_type("B"))
-
-print(factory.get_type("C"))
