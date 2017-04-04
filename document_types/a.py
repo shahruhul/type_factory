94da96ea5e8b301023f7af8b8ea68c80b9cb4152
Ruhul Amin <ruhul@onfido.com>
2017-04-04 16:54:09 +0100

    Improve logic

--- document_types/a.py
+++ document_types/a.py
@@ -9,10 +9,11 @@ class A(object):
 
     def is_valid(cls, values):
 	names = [method for method in dir(cls) if hasattr(getattr(cls, method), 'validator')]
-	functions = [getattr(cls, name) for name in names]
 
         valid = True
-	for func in functions:
+
+        for name in names:
+            func = getattr(cls, name)
             valid = valid and func(values)
 
         return valid
